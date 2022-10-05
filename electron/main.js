ca999a5cb6420eb77532037c793d8a82cd8777e3
Hexerpowers <hexerpowers@gmail.com>
2022-10-05 20:21:35 +0300

    Исправлен стиль кода

--- electron/main.js
+++ electron/main.js
@@ -1,54 +1,49 @@
-// Модули для управления приложением и создания окна
-const {app, BrowserWindow} = require('electron')
-const path = require('path');
-const url = require('url');
-require('./ipcmain');
+const {app, BrowserWindow, Menu} = require('electron')
+const path = require('path')
+const url = require('url')
 
-function createWindow() {
-    // Создаем окно браузера.
-    const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
+const main_menu = [
+    {
+        label: 'Сервис',
+        submenu: [
+            {
+                role: 'quit',
+                label: 'Выход'
+            },
+            {role: 'toggleDevTools'},
+            {type: 'separator'}
+        ]
+    }
+]
 
-    // и загрузить index.html приложения.
-    //mainWindow.loadFile('index.html')
-
-    const startUrl = process.env.ELECTRON_START_URL || url.format({
-        pathname: path.join(__dirname, '../index.html'),
-        protocol: 'file:',
-        slashes: true
-    });
-
-    mainWindow.loadURL(startUrl);
-    // mainWindow.loadURL('http://localhost:3000');
-
-    // Отображаем средства разработчика.
-    // mainWindow.webContents.openDevTools()
-}
-
-// Этот метод вызывается когда приложение инициализируется
-// и будет готово для создания окон.
-// Некоторые API могут использоваться только после возникновения этого события.
+app.disableHardwareAcceleration()
 app.whenReady().then(() => {
-    createWindow()
-
-    app.on('activate', function () {
-        // На MacOS обычно пересоздают окно в приложении,
-        // после того, как на иконку в доке нажали и других открытых окон нету.
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    let win = new BrowserWindow({
+        show: false,
+        useContentSize: true,
+        minimizable: false,
+        title: 'Progress GUI',
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            webSecurity: false
+        },
+        icon: 'Assets/img/logo.png'
     })
+
+
+    const startUrl = process.env.ELECTRON_START_URL || new url.URL(path.join(__dirname, '../index.html'), 'file:')
+    win.loadURL(startUrl).then(r => {
+        console.log("Startup Log: ",r)
+        win.webContents.setFrameRate(60)
+        win.maximize()
+        win.show()
+    });
 })
 
-// Выйти когда все окна закрыты
-app.on('window-all-closed', function () {
-    // Для приложений и строки меню в macOS является обычным делом оставаться
-    // активными до тех пор, пока пользователь не выйдет окончательно используя Cmd + Q
-    if (process.platform !== 'darwin') app.quit()
-})
+const menu = Menu.buildFromTemplate(main_menu)
+Menu.setApplicationMenu(menu)
 
-// В этом файле вы можете включить остальную часть основного процесса вашего приложения
-//  Вы также можете поместить их в отдельные файлы и подключить через require.
+app.on('window-all-closed', () => {
+    app.quit()
+})
